a0fc753c727892d3e36fb2edca2e1a67895a1f85
Eliseo Fuentes <eliseofuentes.rewired@latam.com>
2021-11-16 17:16:50 -0300

    remove reactotron

--- template/index.ts
+++ template/index.ts
@@ -3,9 +3,4 @@ import { AppRegistry } from 'react-native'
 import App from './src/App'
 import { name as appName } from './app.json'
 
-if (__DEV__) {
-  require('./src/Config/ReactotronConfig')
-  console.tron.log('Reactotron configured')
-}
-
 AppRegistry.registerComponent(appName, () => App)
